16b111d25f78d13b70dfae2e04c112420da2ecf0
dsbenghe <dsbenghe@gmail.com>
2021-08-16 22:36:49 +0200

    Update WindowsTools.ps1

--- scripts/WindowsTools.ps1
+++ scripts/WindowsTools.ps1
@@ -1,17 +1,17 @@
 
 # tools to ease life working with Windows
 choco install -y 7zip.install
-#choco install -y dropbox
+choco install -y dropbox
 #choco install -y cmdermini
-#choco install -y keepass
+choco install -y keepass
 # choco install -y sharex
 #choco install -y paint.net
-#choco install -y SwissFileKnife
+choco install -y SwissFileKnife
 choco install -y windirstat
-#choco install -y rufus
+choco install -y rufus
 
 #choco install -y f.lux
 #choco pin add -n=f.lux
-#choco install -y teamviewer
-#choco install -y vlc
-#choco install -y bsplayer
+choco install -y teamviewer
+choco install -y vlc
+choco install -y bsplayer
